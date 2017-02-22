b0be914911751551d2cfd0ce46aa410a0f3c36a6
Duong Do <dohoangduong95@gmail.com>
2017-02-21 22:02:48 -0600

    Add Graph tutorial

---
+++ data/intro.java
@@ -0,0 +1,19 @@
+/*! \mainpage
+
+ *  Class relations expressed via an inline dot graph:
+ *  \dot
+ *  digraph example {
+ *      node [shape=circle, fontname=Helvetica, fontsize=10];
+ *      1 [ label="class 1" URL="md_markdown_file1.html"];
+ *      2 [ label="class 2" URL="md_markdown_file2.html"];
+ *      3 [ label="class 3" URL="md_markdown_file3.html"];
+ *      4 [ label="class 4" URL="md_markdown_file4.html"];
+ *      1 -> 2 [label=""];
+ *      1 -> 3 [label=""];
+ *      2 -> 4 [label=""];
+ *  }
+ *  \enddot
+ *  Note that the classes in the above graph are clickable
+ *  (in the HTML output).
+ */
+ 
